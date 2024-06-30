d478b8bfd2ae0a55408fd2b9735a604c5fb9a139
Daniel Boll <danielboll.academico@gmail.com>
2024-06-30 19:24:08 -0300

    fix(build): add declaration files
    Signed-off-by: Daniel Boll <danielboll.academico@gmail.com>

--- build.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-	entry: ["src/**/*.ts", "example/**/*.ts"],
-	splitting: true,
-	sourcemap: true,
-	clean: true,
-	outDir: "distTsUp",
-});
