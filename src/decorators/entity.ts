f16ded806ee22ed11663d2fcfe37a448159aebb8
Daniel Boll <danielboll.academico@gmail.com>
2024-07-09 19:42:25 -0300

    feat(findBy): add findBy implementation and correct types
    Signed-off-by: Daniel Boll <danielboll.academico@gmail.com>

--- src/decorators/entity.ts
+++ src/decorators/entity.ts
@@ -2,22 +2,22 @@ import { BaseEntity } from "../entity/base";
 import { snakeCaseTransform } from "../utils/snake-case-transform";
 
 interface TableOptions {
-	compaction?: CompactionType;
+  compaction?: CompactionType;
 }
 
 // NOTE: We should watch if this enum shouldn't be moved to the driver itself. It depends on the driver implementation needing the same info.
 enum CompactionType {
-	TimeWindowCompactionStrategy = 0,
-	SizeTieredCompactionStrategy = 1,
-	LeveledCompactionStrategy = 2,
-	IncrementalCompactionStrategy = 3,
+  TimeWindowCompactionStrategy = 0,
+  SizeTieredCompactionStrategy = 1,
+  LeveledCompactionStrategy = 2,
+  IncrementalCompactionStrategy = 3,
 }
 
 export function Entity(tableName?: string, options?: TableOptions) {
-	return (entity: Function) => {
-		entity.prototype.tableName = tableName ?? snakeCaseTransform(entity.name);
-		entity.prototype.__proto__ = BaseEntity.prototype;
+  return (entity: Function) => {
+    entity.prototype.tableName = tableName ?? snakeCaseTransform(entity.name);
+    entity.prototype.__proto__ = BaseEntity.prototype;
 
-		if (options?.compaction) entity.prototype.compaction = options;
-	};
+    if (options?.compaction) entity.prototype.compaction = options;
+  };
 }
--- src/repository/base.ts
+++ src/repository/base.ts
@@ -1,10 +1,16 @@
-import { Uuid } from "@lambda-group/scylladb";
+import type { Uuid } from "@lambda-group/scylladb";
 import type { DataSource } from "../data-source";
-import { ColumnType, FilterQuery, type ColumnDefinition } from "../decorators";
+import type {
+	AnyArray,
+	FilterQuery,
+	ColumnDefinition,
+	NonFilteredQuery,
+} from "../decorators";
+import { ColumnType, PrimaryKeyProp } from "../decorators";
 import type { BaseEntityConstructor } from "../entity";
 import { Logger } from "../utils/logger";
 
-export class Repository<T> {
+export class Repository<T extends { [PrimaryKeyProp]?: AnyArray }> {
 	private dataSource: DataSource;
 	private tableName: string;
 	private columns: ColumnDefinition[];
@@ -61,7 +67,42 @@ export class Repository<T> {
 		});
 	}
 
-	async findBy(props: FilterQuery<T>) {}
+	async findBy(query: FilterQuery<T>): Promise<T[]> {
+		const session = this.dataSource.getSession();
+		const columnNames = this.columns.map((col) => col.columnName).join(", ");
+
+		const whereClause = Object.keys(query)
+			.filter((key) => key !== "allowFiltering")
+			.map((key) => {
+				const column = this.columns.find((col) => col.key === key);
+				if (!column) {
+					this.logger.error(`Column ${key} not found`);
+					throw new Error(`Column ${key} not found`);
+				}
+
+				return `${column.columnName} = ?`;
+			})
+			.join(" AND ");
+
+		// Omit allowFiltering from the query
+		const values = Object.keys(query)
+			.filter((key) => key !== "allowFiltering")
+			.map((key) => query[key as keyof FilterQuery<T>] as NonFilteredQuery<T>);
+		const allowFiltering = query.allowFiltering ? " ALLOW FILTERING" : "";
+
+		const queryStr = `SELECT ${columnNames} FROM ${this.tableName} WHERE ${whereClause} ${allowFiltering}`;
+		this.logger.log(`Executing query: ${queryStr} with values: [${values}]`);
+
+		return (await session.execute(queryStr, values)).map((row: unknown) => {
+			const entity = new this.entityClass();
+			for (const col of this.columns) {
+				// @ts-ignore: Object has no index signature
+				entity[col.key] = this.columnToValue(col, row);
+			}
+
+			return entity;
+		});
+	}
 
 	async findByPartitionKey<U extends string | number | Uuid>(
 		id: U,
